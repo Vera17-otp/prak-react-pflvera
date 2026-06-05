8191906a58210d4d51bac36778f15d4fcb019222
Vera17-otp <vera24si@mahasiswa.pcr.ac.id>
2026-06-05 14:54:20 +0700

    Hai

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -38,7 +38,7 @@ export default function Header({ searchTerm, setSearchTerm }) {
 
                     <div className="flex items-center space-x-4 border-l pl-4 border-gray-300">
                         <span className="hidden md:inline text-sm">
-                            Hello, <span className="font-bold">Vera Zakia</span>
+                            Hai, <span className="font-bold">Vera Zakia</span>
                         </span>
                         <img
                             src="https://cdn-icons-png.flaticon.com/512/4140/4140037.png"
